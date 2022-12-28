4f4992ff1035e2bfcec8bab54355980a388e20f9
Robson Silva <robsantossilva@gmail.com>
2022-12-28 19:28:57 +0000

    feat: :sparkles: - Add abstract value object\n- Add value object for entity ID

--- src/@seedwork/domain/value-objects/__tests__/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/value-objects/__tests__/unique-entity-id.vo.spec.ts
@@ -1,5 +1,5 @@
-import InvalidUuidError from "../errors/invalid-uuid.error";
-import UniqueEntityId from "./unique-entity-id.vo";
+import InvalidUuidError from "../../../errors/invalid-uuid.error";
+import UniqueEntityId from "../unique-entity-id.vo";
 import { validate as uuidValidate } from "uuid";
 
 describe("UniqueEntityId Unit Tests", () => {
@@ -13,13 +13,13 @@ describe("UniqueEntityId Unit Tests", () => {
   it("should accept a uuid passed in constructor", () => {
     const uuid = "805498ae-bec3-46b8-ba87-c0fd403f8cd0";
     const vo = new UniqueEntityId(uuid);
-    expect(vo.id).toBe(uuid);
+    expect(vo.value).toBe(uuid);
     expect(validateSpy).toHaveBeenCalledTimes(1);
   });
 
   it("should generate a new uuid when none is passed in constructor", () => {
     const vo = new UniqueEntityId();
-    expect(uuidValidate(vo.id)).toBeTruthy();
+    expect(uuidValidate(vo.value)).toBeTruthy();
     expect(validateSpy).toHaveBeenCalledTimes(1);
   });
 });
