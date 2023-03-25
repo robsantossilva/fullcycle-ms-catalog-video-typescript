c0507a3701ed42e03017428e86733cf92bff6777
Robson Silva <robsantossilva@gmail.com>
2023-03-25 12:17:17 -0300

    Tests InMemoryRepository parte 2

--- src/@seedwork/domain/repository/in-memory.repository.spec.ts
+++ src/@seedwork/domain/repository/in-memory.repository.spec.ts
@@ -54,4 +54,61 @@ describe("InMemoryRepository Unit Tests", () => {
     entityFound = await repository.findById(entity.uniqueEntityId);
     expect(entity.toJSON()).toStrictEqual(entityFound.toJSON());
   });
+
+  it("should returns all entities", async () => {
+    const entity = new StubEntity({ name: "name value", price: 5 });
+    await repository.insert(entity);
+
+    const entities = await repository.findAll();
+    expect(entities).toStrictEqual([entity]);
+  });
+
+  it("should throws error on update when entity is not found", async () => {
+    const entity = new StubEntity({ name: "name value", price: 5 });
+
+    expect(repository.update(entity)).rejects.toThrow(
+      new NotFoundError(`Entity Not Found using ID ${entity.id}`)
+    );
+  });
+
+  it("should updates an entity", async () => {
+    const entity = new StubEntity({ name: "name value", price: 5 });
+    await repository.insert(entity);
+
+    const entityUpdated = new StubEntity(
+      { name: "updated", price: 1 },
+      entity.uniqueEntityId
+    );
+    await repository.update(entityUpdated);
+    expect(entityUpdated.toJSON()).toStrictEqual(repository.items[0].toJSON());
+  });
+
+  it("should throws error on delete when entity is not found", async () => {
+    expect(repository.delete("fake_id")).rejects.toThrow(
+      new NotFoundError(`Entity Not Found using ID fake_id`)
+    );
+
+    expect(
+      repository.delete(
+        new UniqueEntityId("c31e7d81-d17c-4cd1-aace-cd9004d9668c")
+      )
+    ).rejects.toThrow(
+      new NotFoundError(
+        `Entity Not Found using ID c31e7d81-d17c-4cd1-aace-cd9004d9668c`
+      )
+    );
+  });
+
+  it("should deletes an entity", async () => {
+    const entity = new StubEntity({ name: "name value", price: 5 });
+    await repository.insert(entity);
+
+    await repository.delete(entity.id);
+    expect(repository.items).toHaveLength(0);
+
+    await repository.insert(entity);
+
+    await repository.delete(entity.uniqueEntityId);
+    expect(repository.items).toHaveLength(0);
+  });
 });
